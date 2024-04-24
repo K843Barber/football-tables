3df4d82adad35a44a808b3276ae0d8df3bdbdabd
k843barber <ka_barber@live.co.uk>
2024-04-24 19:45:37 +0200

    Refactor Football

---
+++ src/football/__main__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+
+"""Add a configuration file, so we have some generics to store."""
+
+import argparse
+from json import load
+
+import argcomplete
+from argcomplete.completers import ChoicesCompleter
+from get_table import get_table
+from show_table import enrich_table, give_dataframe
+
+with open(".config/configuration.json") as f:
+    choices = tuple(load(f)['league'])
+
+mainParser = argparse.ArgumentParser()
+subparsers = mainParser.add_subparsers(dest='command')
+
+# --------------- Get ---------------
+get = subparsers.add_parser('get')
+get.add_argument('--league', 
+                help='Choose league').completer = ChoicesCompleter(choices) 
+get.add_argument('--season', 
+                 nargs=2, 
+                 help='Choose season')
+argcomplete.autocomplete(get)
+
+# --------------- Show ---------------
+show = subparsers.add_parser('show')
+show.add_argument('--league', 
+                  help='Select league').completer = ChoicesCompleter(choices)
+show.add_argument('--season', 
+                  nargs=2, 
+                  help='Select season')
+
+argcomplete.autocomplete(show)
+args = mainParser.parse_args()
+
+def main():
+    a1, a2, a3 = args.league, args.season[0], args.season[1]
+
+    if args.command == "get":
+        get_table(a1, a2, a3)
+    elif args.command == "show":
+        df = give_dataframe(a1,a2,a3)
+        enrich_table(df, a1, a2, a3)
+
+if __name__ == "__main__":
+    main()
---
+++ src/football/config.py
@@ -0,0 +1,13 @@
+from json import dump
+
+from inquirer import Checkbox, prompt
+
+q = [
+    Checkbox("league",
+         message="Which leagues would you like to see?",
+         choices=["Bundesliga", "La_Liga", "Ligue_1", "Premier_League", "Serie_A"])
+]
+
+with open (".config/configuration.json", "w", encoding="utf-8") as f:
+    dump(prompt(q), f, indent=4)
+
