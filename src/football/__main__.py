7bfd193a3660ea97ce00524545d847f50a9dfaae
k843barber <ka_barber@live.co.uk>
2024-04-24 19:54:37 +0200

    Add dependencies

--- src/football/__main__.py
+++ src/football/__main__.py
@@ -8,34 +8,35 @@ from json import load
 
 import argcomplete
 from argcomplete.completers import ChoicesCompleter
-from get_table import get_table
-from show_table import enrich_table, give_dataframe
+
+from .get_table import get_table
+from .show_table import enrich_table, give_dataframe
 
 with open(".config/configuration.json") as f:
     choices = tuple(load(f)['league'])
 
-mainParser = argparse.ArgumentParser()
-subparsers = mainParser.add_subparsers(dest='command')
+main_parser = argparse.ArgumentParser()
+subparsers = main_parser.add_subparsers(dest='command')
 
 # --------------- Get ---------------
 get = subparsers.add_parser('get')
-get.add_argument('--league', 
-                help='Choose league').completer = ChoicesCompleter(choices) 
-get.add_argument('--season', 
-                 nargs=2, 
+get.add_argument('--league',
+                help='Choose league').completer = ChoicesCompleter(choices)
+get.add_argument('--season',
+                 nargs=2,
                  help='Choose season')
 argcomplete.autocomplete(get)
 
 # --------------- Show ---------------
 show = subparsers.add_parser('show')
-show.add_argument('--league', 
+show.add_argument('--league',
                   help='Select league').completer = ChoicesCompleter(choices)
-show.add_argument('--season', 
-                  nargs=2, 
+show.add_argument('--season',
+                  nargs=2,
                   help='Select season')
 
 argcomplete.autocomplete(show)
-args = mainParser.parse_args()
+args = main_parser.parse_args()
 
 def main():
     a1, a2, a3 = args.league, args.season[0], args.season[1]
